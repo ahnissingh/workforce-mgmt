2b9ab8ee79734a037add961d6f25e07d1a0b5960
Ahnis <ahnisaneja@gmail.com>
2025-08-03 17:21:14 +0530

    Added snake_case naming to remaining dto's

--- src/main/java/com/railse/hiring/workforcemgmt/dto/AddCommentRequest.java
+++ src/main/java/com/railse/hiring/workforcemgmt/dto/AddCommentRequest.java
@@ -1,5 +1,9 @@
 package com.railse.hiring.workforcemgmt.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AddCommentRequest(
         Long taskId,
         String message
--- src/main/java/com/railse/hiring/workforcemgmt/dto/UpdateTaskPriorityRequest.java
+++ src/main/java/com/railse/hiring/workforcemgmt/dto/UpdateTaskPriorityRequest.java
@@ -1,8 +1,12 @@
 package com.railse.hiring.workforcemgmt.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.railse.hiring.workforcemgmt.model.enums.Priority;
 
 //Dto which was not defined from codebase given in the pdf I am making use of records for efficiency
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+
 public record UpdateTaskPriorityRequest(
         Long taskId,
         Priority newPriority
