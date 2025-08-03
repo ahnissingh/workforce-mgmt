e4811017ea8fe6593ad506f67a1cb241ce6a5ff6
Ahnis <ahnisaneja@gmail.com>
2025-08-03 15:29:14 +0530

    Added Lombok constructor annotations to TaskFetchByDateRequest and @Builder.Defaults to TaskManagement.java

--- src/main/java/com/railse/hiring/workforcemgmt/dto/TaskFetchByDateRequest.java
+++ src/main/java/com/railse/hiring/workforcemgmt/dto/TaskFetchByDateRequest.java
@@ -2,12 +2,16 @@ package com.railse.hiring.workforcemgmt.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskFetchByDateRequest {
     private Long startDate;
     private Long endDate;
--- src/main/java/com/railse/hiring/workforcemgmt/model/TaskManagement.java
+++ src/main/java/com/railse/hiring/workforcemgmt/model/TaskManagement.java
@@ -4,14 +4,18 @@ import com.railse.hiring.workforcemgmt.common.model.enums.ReferenceType;
 import com.railse.hiring.workforcemgmt.model.enums.Priority;
 import com.railse.hiring.workforcemgmt.model.enums.Task;
 import com.railse.hiring.workforcemgmt.model.enums.TaskStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskManagement {
     private Long id;
     private Long referenceId;
@@ -24,7 +28,9 @@ public class TaskManagement {
     private Priority priority;
 
     //For feature 3
+    @Builder.Default
     private List<Activity> activityHistory = new ArrayList<>();
+    @Builder.Default
     private List<Comment> comments = new ArrayList<>();
 
 }
